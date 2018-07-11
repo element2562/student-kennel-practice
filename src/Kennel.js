06feb57deb0284f9dbd4783f301ff1f2240f2148
Jacob Henderson <element25668@gmail.com>
2018-07-11 10:52:38 -0500

    first commit, done

---
+++ src/Kennel.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import EmployeeList from "./EmployeeList"; // Import EmployeeList component
+import LocationList from "./LocationList";
+export default class Kennel extends Component {
+  render() {
+    return (
+      <div>
+        <LocationList />
+        <EmployeeList />
+      </div>
+    );
+  }
+}
---
+++ src/LocationList.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class LocationList extends Component {
+  render() {
+    return (
+      <article>
+        <h3>Student Kennels</h3>
+        <h4>Nashville North Location</h4>
+        <h5>500 Puppy Way</h5>
+        <h3>New Kennel</h3>
+        <h4>Nashville Software School</h4>
+        <h5>100 main st</h5>
+      </article>
+    );
+  }
+}
+export default LocationList;
